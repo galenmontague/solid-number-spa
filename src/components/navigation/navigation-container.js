f800a702f5823b68f1e2828d114b6191ca985d86
Derek Gilbert <derek@thedevelopmentphase.com>
2019-03-01 10:37:52 -0700

    changes

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { withRouter } from 'react-router'
 import { NavLink } from 'react-router-dom'
-import logo from '../../../static/assets/solidnumber.png'
+import logo from '../../../static/assets/winknaturals.png'
 
 const NavigationComponent = props => {
   return (
-    <div className="nav-wrapper">
-      <div className="left-side">
-        <img src={ logo } height='25'/>
-      </div>
-      <div className="right-side">
-        <div className="nav-link-wrapper">
-          <NavLink to='/dashboard' activeClassName='nav-link-active'> Dashboard</NavLink>
+    <div className="nav-width">
+      <div className="nav-wrapper">
+        <div className="left-side">
+          <img src={ logo } height="75"/>
         </div>
-        <div className="nav-link-wrapper">
-          <NavLink to='/settings' activeClassName='nav-link-active'> Settings</NavLink>
+        <div className="right-side">
+          <div className="nav-link-wrapper">
+            <NavLink to='/dashboard' activeClassName='nav-link-active'> Dashboard</NavLink>
+          </div>
+          <div className="nav-link-wrapper">
+            <NavLink to='/settings' activeClassName='nav-link-active'> Settings</NavLink>
+          </div>
         </div>
       </div>
     </div>
